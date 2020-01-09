3b4ec83cf7e45a82c265d9c60fa858771c37cb41
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-01-09 23:07:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,7 +56,7 @@ class Song
     new_song = Song.new
     new_song.artist_name = file_name.split("-")[0].strip.to_s
     new_song.name = file_name.split("-")[1].to_s.split(".")[0].strip.to_s
-    new_song
+    @@all << new_song
   end
 
 end
