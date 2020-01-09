d9194f85a77767b8b5f63ac16d2422bb37beaf46
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-01-09 23:07:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
     new_song
   end
 
-  def self.create_from_filename
+  def self.create_from_filename(file_name)
     new_song = Song.new
     new_song.artist_name = file_name.split("-")[0].strip.to_s
     new_song.name = file_name.split("-")[1].to_s.split(".")[0].strip.to_s
