0c209983e808c498c140612eef2aa25dc45969a9
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-01-09 22:28:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
 
   def self.find_or_create_by_name(string_name)
     if self.find_by_name(string_name)
-      return string_name.name
+      return string_name
     else
       self.create_by_name(string_name)
     end
