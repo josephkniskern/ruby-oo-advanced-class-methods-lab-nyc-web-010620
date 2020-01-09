0ee36e5e9ffc87faa38b5b3cec7372af20bb5e2f
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-01-09 22:05:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,46 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
   def self.all
     @@all
+
   end
 
   def save
     self.class.all << self
   end
+
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    @@all << song_name
+    song
+  end
+
+  def self.find_by_name(string_name)
+    # binding.pry
+    @@all.find do |n|
+      n.name == string_name
+    end
+    # return false
+    binding.pry
+
+  end
+
 
 end
